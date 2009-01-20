a233755f4ce05f26eec42224f31581717b4aad8c
scorlosquet <scorlosquet@93ccd116-59cd-4aef-8adb-366cfb8b6e0f>
2009-01-20 15:49:01 +0000

    initial import of the XSPARQL REST API. requires django.

---
+++ xsparql/trunk/src/xsparqlrest/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^xsparqlrest/', include('xsparqlrest.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^xsparql/', 'xsparqlrest.restapi.views.query'),
+    (r'^', 'xsparqlrest.restapi.views.index'),
+)
