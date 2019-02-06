bbd990b740e4ea955bdfb20342d069667e66bf61
Zachary <zachary.whitley@gmail.com>
2019-02-06 07:01:41 -0500

    Refactor TreePrinter

--- xsparql-rewriter/src/main/java/org/sourceforge/xsparql/rewriter/TreePrinter.java
+++ xsparql-rewriter/src/main/java/org/sourceforge/xsparql/rewriter/TreePrinter.java
@@ -3,51 +3,47 @@ package org.sourceforge.xsparql.rewriter;
 import org.antlr.runtime.tree.Tree;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.PrintStream;
 
 public class TreePrinter {
-    private int dirCount;
-    private int fileCount;
-    private ByteArrayOutputStream os = new ByteArrayOutputStream();
-    private PrintStream ps = new PrintStream(os);
 
-    public TreePrinter() {
-        this.dirCount = 0;
-        this.fileCount = 0;
-    }
+    private final ByteArrayOutputStream os = new ByteArrayOutputStream();
+    private final PrintStream ps = new PrintStream(os);
 
-    public String print(Tree tree) {
+    public String print(final Tree tree) {
+
         walk(tree, "");
         return os.toString();
     }
 
-    private void register(Tree file) {
-        if (file.getChildCount() != 0) {
-            this.dirCount += 1;
-        } else {
-            this.fileCount += 1;
+    private void walk(final Tree parent, final String prefix) {
+
+        for (int index = 0; index < parent.getChildCount(); index++) {
+            final Tree child = parent.getChild(index);
+
+            if (index == parent.getChildCount() - 1) {
+                ps.println(prefix + "└── " + getText(child));
+                if (hasChildren(parent)) {
+                    walk(child, prefix + "    ");
+                }
+            } else {
+                ps.println(prefix + "├── " + getText(child));
+                if (hasChildren(parent)) {
+                    walk(child, prefix + "│   ");
+                }
+            }
         }
     }
 
-    private void walk(Tree folder, String prefix) {
-        Tree file;
+    private boolean hasChildren(final Tree tree) {
+        return tree.getChildCount() == 0 ? false : true;
+    }
 
-        for (int index = 0; index < folder.getChildCount(); index++) {
-            file = folder.getChild(index);
-            register(file);
-
-            if (index == folder.getChildCount() - 1) {
-                ps.println(prefix + "└── " + file.getText());
-                if (file.getChildCount() != 0) {
-                    walk(file, prefix + "    ");
-                }
-            } else {
-                ps.println(prefix + "├── " + file.getText());
-                if (file.getChildCount() != 0) {
-                    walk(file, prefix + "│   ");
-                }
-            }
+    private String getText(final Tree tree) {
+        if(tree.getText() == null) {
+            return "NULL";
+        } else {
+            return tree.getText().replaceAll("\n", "\\\\n");
         }
     }
 }
